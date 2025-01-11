b4ff8d3a74bd0a7fe3eb3dba4ad675d673bf748b
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-11 08:58:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,5 +16,5 @@ ADMINS = int(os.environ.get("ADMINS", ""))
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', Flase))
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then False
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', False))
