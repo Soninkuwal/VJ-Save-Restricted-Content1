dcd7f0d337c12ad50434d077bb07bbd435919593
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-19 00:36:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,3 +11,12 @@ API_HASH = os.environ.get("API_HASH", "")
 
 #Database 
 DB_URI = os.environ.get("DB_URI", "")
+
+# Admin and Log
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+# Features
+ERROR_MESSAGE = True
+AUTO_FORWARD_CHANNEL = os.environ.get("AUTO_FORWARD_CHANNEL")
+DEFAULT_REACTIONS = ["👍", "❤️", "🔥"] #Default reactions for the bot on messages
