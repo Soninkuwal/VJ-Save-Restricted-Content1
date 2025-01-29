7914ddf1fd783b27efba54494b9c2b88bef2bb72
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-29 16:29:59 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
+
 class Database:
     
     def __init__(self, uri, database_name):
@@ -13,6 +14,7 @@ class Database:
             id = id,
             name = name,
             session = None,
+            forward_channel_id=None,
         )
     
     async def add_user(self, id, name):
@@ -38,6 +40,41 @@ class Database:
 
     async def get_session(self, id):
         user = await self.col.find_one({'id': int(id)})
-        return user['session']
+        return user.get('session')
+
+    async def set_forward_channel(self, user_id: int, forward_channel_id: int):
+        await self.col.update_one({'id': int(user_id)}, {'$set': {'forward_channel_id': forward_channel_id}})
+
+    async def get_forward_channel(self, user_id: int):
+        user = await self.col.find_one({'id': int(user_id)})
+        return user.get('forward_channel_id')
+
+    async def add_replace_word(self, old_word: str, new_word: str):
+         replace_col = self.db.replace_words
+         await replace_col.insert_one({"old_word": old_word, "new_word": new_word})
+      
+    async def get_replace_words(self):
+         replace_col = self.db.replace_words
+         cursor = replace_col.find({})
+         return await cursor.to_list(length=None)
+
+    async def remove_replace_word(self, old_word: str):
+      replace_col = self.db.replace_words
+      await replace_col.delete_one({"old_word":old_word})
+
+    async def add_delete_word(self, word: str):
+        delete_col = self.db.delete_words
+        await delete_col.insert_one({"word": word})
+
+    async def get_delete_words(self):
+         delete_col = self.db.delete_words
+         cursor = delete_col.find({})
+         result = await cursor.to_list(length=None)
+         return [row.get("word") for row in result ]
+
+    async def remove_delete_word(self, word: str):
+        delete_col = self.db.delete_words
+        await delete_col.delete_one({"word": word})
+
 
 db = Database(DB_URI, DB_NAME)
